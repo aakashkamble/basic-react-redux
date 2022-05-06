acd8fe8fd39c7af93f381c92a55d4cee76d9d019
Akash <akashkamble7218@gmail.com>
2022-05-06 14:37:02 +0530

    Basic React Redux

---
+++ src/redux/Store.js
@@ -0,0 +1,5 @@
+// import { createStore } from 'redux';
+import { reducer } from './reducer';
+import { legacy_createStore as createStore} from 'redux'
+
+export const Store =  createStore(reducer,{})
